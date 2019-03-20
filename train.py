231acb488d78e87a0523abc9000db77f3a7b238f
Dian <0856dian@gmail.com>
2019-03-20 15:17:15 +0700

    edit to own data (vehicle)

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ from torch.autograd import Variable
 import torch.optim as optim
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--epochs", type=int, default=30, help="number of epochs")
+parser.add_argument("--epochs", type=int, default=1000, help="number of epochs")
 parser.add_argument("--image_folder", type=str, default="data/samples", help="path to dataset")
 parser.add_argument("--batch_size", type=int, default=16, help="size of each image batch")
 parser.add_argument("--model_config_path", type=str, default="config/yolov3.cfg", help="path to model config file")
